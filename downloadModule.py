901dda46143fb8d2415751fb722099dc2a729ae9
guilherme <jguicf@gmail.com>
2023-04-30 13:08:58 -0400

    saving already dowloaded ipas

--- downloadModule.py
+++ downloadModule.py
@@ -2,6 +2,22 @@ import requests
 import subprocess
 import json
 
+def saveBundleIdentifier(bundleIdentifier):
+    file = open("Bundles_identifiers.txt", "a")
+    file.write(bundleIdentifier + "\n")
+    file.close()
+
+def isDownloaded(bundleIdentifier):
+    file = open("Bundles_identifiers.txt", "r")
+    isBundleInFile = False
+
+    for line in file:
+        if bundleIdentifier in line:
+            isBundleInFile = True
+            break
+    file.close()
+    return isBundleInFile
+
 def downloadAppStore():
     response = requests.get("https://rss.applemarketingtools.com/api/v2/br/apps/top-free/50/apps.json")
     appInfos = response.json()['feed']['results']
@@ -16,14 +32,17 @@ def downloadAppStore():
         jsonOutput = json.loads("{" + stringJson+ "}")
 
         bundleIdentifier = jsonOutput["bundleID"]
-        try:
-            result = subprocess.check_output(["ipatool", "purchase", "-b", bundleIdentifier])
-        except subprocess.CalledProcessError as e:
-            print("Liscença já obtida ou falha na liscença")
 
-        try:
-            result = subprocess.run(["ipatool", "download", "-b", bundleIdentifier, "-o", "./apps"])
-        except:
-            print("Falha no download da aplicação")
+        if not isDownloaded(bundleIdentifier):
+            try:
+                result = subprocess.check_output(["ipatool", "purchase", "-b", bundleIdentifier])
+            except subprocess.CalledProcessError as e:
+                print("Liscença já obtida ou falha na liscença")
+
+            try:
+                result = subprocess.run(["ipatool", "download", "-b", bundleIdentifier, "-o", "./apps"])
+                saveBundleIdentifier(bundleIdentifier)
+            except:
+                print("Falha no download da aplicação")
 
 downloadAppStore()
